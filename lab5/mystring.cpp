b528ea9b257d3a538a205f924d5df0631caac626
Zach Azer <zazer24@gmail.com>
2021-05-03 15:57:47 -0700

    committing

--- lab5/mystring.cpp
+++ lab5/mystring.cpp
@@ -1,4 +1,16 @@
-// AUTHOR: James Dameris
+// AUTHOR: Zach Azer
 // FILE: mystring.cpp
 // CLASS IMPLEMENTED: mystring (see mystring.h for documentation)
+#include <cassert>   // Provides assert
+#include <iostream>  // Provides istream class
+#include <cstring>
+#include "mystring.h"
 
+using namespace std;
+
+namespace coen79_lab5{
+    string::string(const char str[ ] = ""){
+        assert(str == '\0');
+        
+    }
+}

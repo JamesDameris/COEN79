2ff041eeefc790da713550f7348758fc4dc691da
JamesDameris <jamesdameris@gmail.com>
2021-05-03 15:41:36 -0700

    initialized operator +=

--- lab5/mystring.cpp
+++ lab5/mystring.cpp
@@ -2,3 +2,12 @@
 // FILE: mystring.cpp
 // CLASS IMPLEMENTED: mystring (see mystring.h for documentation)
 
+void operator +=(const string& addend) {
+    
+}
+void operator +=(const char addend[ ]) {
+
+}
+void operator +=(char addend) {
+
+}

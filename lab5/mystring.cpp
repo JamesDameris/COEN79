84aa707baa086fe84702b1dfca1c89e7e65847ed
Zach Azer <zazer24@gmail.com>
2021-05-04 23:43:03 -0700

    +

--- lab5/mystring.cpp
+++ lab5/mystring.cpp
@@ -171,12 +171,10 @@ namespace coen79_lab5
 
     string operator +(const string& s1, const string& s2) {
         string s;
-        char c = '\0';
-        size_t total = s1.length() + s2.length();
+        size_t total = s1.length() + s2.length() + 1;
         s.reserve(total);
-        size_t end = s1.search(c);
-        string str = s.replace(s2, end);
-
+        s += s1;
+        s += s2;
     }
 
     string operator +(const string& s1, const char addend[ ]) {

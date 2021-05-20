f1226bae8d6cc1ca1daf6670891074a0bec698dd
Zach Azer <zazer24@gmail.com>
2021-05-20 16:05:46 -0700

    company comments

--- lab7/company.cpp
+++ lab7/company.cpp
@@ -15,27 +15,35 @@
 
 namespace coen79_lab7
 {
+/* Function: constructor
+ * Description: initializes a company object with an empty name
+*/ 
     company::company() {
         this->company_name = "";
         this->head_ptr = NULL;
         this->tail_ptr = NULL;
     }
-    
+/* Function: constructor
+ * Description: initializes a company object with an inputted name
+*/ 
     company::company(const std::string& company_name) {
         assert(company_name.length() > 0);
         this->company_name = company_name;
         this->head_ptr = NULL;
         this->tail_ptr = NULL;
     }
-    
+/* Function: copy constructor
+ * Description: creates the copy constructor for the company class
+*/ 
     company::company(const company &src) {
         Debug("Company copy constructor..." << std::endl);
         this->head_ptr = NULL;
         this->tail_ptr = NULL;
         *this = src;
     }
-
-    
+/* Function: assignment operator
+ * Description: creates the assignment operator for the company class
+*/ 
     company& company::operator= (const company &src) {
         Debug("Company assignemnt operator..." << std::endl);
         if (this == &src) {
@@ -46,38 +54,52 @@ namespace coen79_lab7
         list_copy(src.head_ptr, head_ptr, tail_ptr);
         return *this;
     }
-    
-    
+/* Function: destructor
+ * Description: clears the list and its memory
+*/ 
     company::~company() {
         list_clear(head_ptr);
     }
-    
-    
+/* Function: get name
+ * Description: returns the name of the company
+*/ 
     std::string company::get_name() const {
         return company_name;
     }
-    
+/* Function: get head
+ * Description: returns the constant version of the head of the linked list
+*/ 
     const node *company::get_head() const {
         return head_ptr;
     }
-    
+/* Function: get tail
+ * Description: returns the constant version of the tail of the linked list
+*/ 
     const node *company::get_tail() const {
         return tail_ptr;
     }
-    
+/* Function: get head
+ * Description: returns the non constant version of the head of the linked list
+*/ 
     node* company::get_head() {
         return head_ptr;
     }
-    
+/* Function: get tail
+ * Description: returns the non constant version of the tail of the linked list
+*/ 
     node* company::get_tail() {
         return tail_ptr;
     }
-    
+/* Function: print items
+ * Description: prints the list
+*/ 
     void company::print_items() {
         list_print(head_ptr);
     }
-    
-    
+/* Function: insert
+ * Description: creates and inserts a new node to the back of the list
+ * Runtime: O(1)
+*/ 
     bool company::insert(const std::string& product_name, const float& price) {
         
         assert(product_name.length() > 0);
@@ -96,7 +118,10 @@ namespace coen79_lab7
         
         return true;
     }
-
+/* Function: erase
+ * Description: erases a product matching the given name
+ * Runtime: O(n)
+*/ 
     
     bool company::erase(const std::string& product_name) {
         assert(product_name.length() > 0);
